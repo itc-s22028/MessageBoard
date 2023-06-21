ada21340fc5b70546beb517ca1bb2fc629f98a3c
Nakasone, Tsuyoshi <nakasone@std.it-college.ac.jp>
2023-06-21 14:35:57 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# s22 Kotlin 課題
+
+必ずこのリポジトリをフォークしてから取り掛かってください。
+
+## 事前準備
+1. Docker のコンテナで使うボリュームを事前に作成してください。
+   1. mb_pg_data
+   2. mb_redis_data
+2. docker-compose.yml を使用して起動する前に、
+BookManager で使用したコンテナ一式は停止させてください。
+3. データベースの情報は以下の通りで作成してください
+
+   データベース名
+   :  message_board
+
+   接続ユーザ名
+   :  kotlin
+
+   ユーザパスワード
+   :  spring
+
+4. テーブルの作成は以下のSQL文を参照してください。
+   ```postgresql
+   CREATE TABLE users (
+       id SERIAL PRIMARY KEY ,
+       username VARCHAR(64) NOT NULL UNIQUE ,
+       password VARCHAR(128) NOT NULL
+   );
+   
+   CREATE TABLE threads (
+       id BIGSERIAL PRIMARY KEY ,
+       title VARCHAR(256) NOT NULL ,
+       created_at TIMESTAMP NOT NULL,
+       updated_at TIMESTAMP
+   );
+   
+   CREATE TABLE messages (
+       id BIGSERIAL PRIMARY KEY ,
+       user_id BIGINT NOT NULL ,
+       message TEXT ,
+       created_at TIMESTAMP NOT NULL,
+       updated_at TIMESTAMP
+   )
+   ```
---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "3.1.0"
+    id("io.spring.dependency-management") version "1.1.0"
+    id("com.thinkimi.gradle.MybatisGenerator") version "2.4.1"
+    kotlin("jvm") version "1.8.21"
+    kotlin("plugin.spring") version "1.8.21"
+}
+
+group = "jp.ac.it_college.std.s22000"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.2")
+    implementation("org.springframework.session:spring-session-data-redis")
+    implementation("redis.clients:jedis")
+    implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.5.0")
+    mybatisGenerator("org.mybatis.generator:mybatis-generator-core:1.4.2")
+    runtimeOnly("org.postgresql:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+mybatisGenerator {
+    verbose = true
+    configFile = "${projectDir}/src/main/resources/generatorConfig.xml"
+}
